8db73a34fae6860aacd8d90856df9118c0a5655c
Pyrolusite_PC <kasenth@gmail.com>
2014-05-06 01:24:15 +0200

    Added a config option to change healing duration

--- 1.7.2/java/pyrolusite/diamondcookies/DiamondCookies.java
+++ 1.7.2/java/pyrolusite/diamondcookies/DiamondCookies.java
@@ -3,8 +3,10 @@ package pyrolusite.diamondcookies;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
  
@@ -13,15 +15,24 @@ public class DiamondCookies
 {
 
     public static final String MODID = "diamondcookies";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     public static ItemFood diamondCookie;
+    public static int healthRegenDuration;
+   
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+
+    	config.load();
+    	healthRegenDuration = config.get(Configuration.CATEGORY_GENERAL, "Healing duration (1.25 sec = half an heart of healing, but duration number MUST be integer)", 3).getInt();
+    	config.save();
+    }
     
     @EventHandler
-    public void preinit(FMLPreInitializationEvent event)
+    public void init(FMLInitializationEvent event)
     {
-    	
-    	diamondCookie = new ItemFoodDiamondCookie();
+    	diamondCookie = new ItemFoodDiamondCookie(healthRegenDuration);
     	GameRegistry.registerItem(diamondCookie, DiamondCookies.MODID + "_" + diamondCookie.getUnlocalizedName().substring(5));
     	GameRegistry.addRecipe(new ItemStack(DiamondCookies.diamondCookie, 8), new Object[] {
 	        "CCC",
--- 1.7.2/java/pyrolusite/diamondcookies/ItemFoodDiamondCookie.java
+++ 1.7.2/java/pyrolusite/diamondcookies/ItemFoodDiamondCookie.java
@@ -6,13 +6,15 @@ import net.minecraft.potion.Potion;
 
 public class ItemFoodDiamondCookie extends ItemFood
 {
-	public ItemFoodDiamondCookie()
+	public ItemFoodDiamondCookie(int regenDuration)
 	{
-		super(4, false); // par1 = int, hunger points restored ; par2 = bool, saturation
+		// par1 = int, hunger points restored ; par2 = bool, saturation
+		super(4, false);
 		this.setUnlocalizedName("diamondCookie");
 		this.setTextureName(DiamondCookies.MODID + ":" + getUnlocalizedName().substring(5));
 		this.setCreativeTab(CreativeTabs.tabFood);
-		this.setPotionEffect(Potion.regeneration.id, 2, 1, 1F);
+		// par1 = potion id, par2 = int, duration, par3 = int, amplifier (buff level), par4 = float, probablilty
+		this.setPotionEffect(Potion.regeneration.id, regenDuration, 1, 1F);
 		this.setAlwaysEdible();
 	}
 }
